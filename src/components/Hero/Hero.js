bdde2ad8b47bfe368bfc448c26e3342b446755b1
rakshithGGowda <rakshithgowdag89@gmail.com>
2024-06-08 15:41:47 +0530

    hero content update

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -11,14 +11,14 @@ const Hero = (props) => (
   <Section row nopadding><br/>
 
     <LeftSection >
-      <SectionTitle main center ><div className="animate__animated animate__fadeInLeft">Hey.. <br/></div>
+      <SectionTitle main center ><div className="animate__animated animate__fadeInLeft">Hey! <br/></div>
       <span className="animate__animated animate__fadeInUp animate__delay-1s">I'm</span> <span className="animate__animated animate__fadeInUp" style={{animationDelay:"1.5s"}}>Rakshith<br/></span>
       <span className="animate__animated animate__fadeInUp" style={{animationDelay:"2s"}} >A</span> <span className="animate__animated animate__fadeInUp" style={{animationDelay:"2.2s"}} >Machine Learning</span> <span className="animate__animated animate__fadeInUp" style={{animationDelay:"2.7s"}} >Engineer</span>
       </SectionTitle>
       <SectionText>
-      <span className="animate__animated animate__flipInX animate__delay-3s">As a Machine Learning Engineer and Software Developer with over three years of experience in developing innovative solutions to develop Artificial intelligence</span>
-       <span className="animate__animated animate__flipInX" style={{animationDelay:"3.4s"}} >I intend to build my career developing software that matters.</span> <span className="animate__animated animate__flipInX" style={{animationDelay:"3.7s"}} >
-       My expertise encompasses a wide range of areas, including machine learning model research/deployment, generative AI, and full-stack web application development.</span>
+      <span className="animate__animated animate__flipInX animate__delay-3s">With over three years of experience, I specialize in creating innovative solutions for artificial intelligence </span>
+       <span className="animate__animated animate__flipInX" style={{animationDelay:"3.4s"}} > model development and user-centric web applications.</span> <span className="animate__animated animate__flipInX" style={{animationDelay:"3.7s"}} >
+       My expertise spans a diverse array of domains, including machine learning model research and deployment, generative AI, and full-stack web application development.</span>
       
       </SectionText>
       <div className="animate__animated animate__fadeIn" style={{animationDelay:"4.5s", animationDuration: "2s"}} >
